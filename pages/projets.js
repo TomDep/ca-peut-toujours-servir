95cc0aabe16e96c8f82045f38981696007f51536
Tom de Pasquale <tomarthus72@gmail.com>
2022-08-30 12:18:57 +0200

    Adding the projects page

--- pages/projets.js
+++ pages/projets.js
@@ -1,5 +1,43 @@
-export default function Projets() {
+import Link from "next/link";
+import Header from "../components/header";
+import Layout from "../components/layout";
+import { getProjectsProperties } from "../lib/projects";
+
+import styles from "./projets.module.scss";
+
+export async function getStaticProps() {
+    // Get the projects names and ids
+    const properties = getProjectsProperties();
+
+    const projects = properties.map((property) => {
+        return {
+            id: property.id,
+            name: property.name
+        }
+    })
+
+    console.log(projects)
+
+    return {
+        props : {
+            projects
+        }
+    }
+}
+
+export default function Projets({ projects }) {
     return (
-        <h1>Les projets</h1>
+        <>
+            <Header></Header>
+            <Layout>
+                <div className={styles.container}>
+                    {projects.map((project, index) => (
+                        <Link href={'/projets/' + project.id} key={index}>
+                            <a>{project.name}</a>
+                        </Link>
+                    ))}
+                </div> 
+            </Layout>
+        </>
     );
 }
